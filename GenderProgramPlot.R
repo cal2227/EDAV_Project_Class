0d2334f4faaf94c64709cc5a9dabe563a9efb22a
SrividyaKannan <sk3957@columbia.edu>
2016-01-31 10:24:30 -0500

    Adding in the code to produce a plot by Gender and Program and updated ReadME.Rmd

---
+++ GenderProgramPlot.R
@@ -0,0 +1,24 @@
+#What is your directory and filename
+dirPath<-"C:/Users/Srividya/Desktop/EDAV/HW1"
+setwd(dirPath)
+filename<-"Survey+Response.xlsx"
+source("tidydata.R")
+d<-tidydata(filename)
+
+library(ggplot2)
+#Plotting stacked bar plot to show distribution of students in class by program and gender
+
+#Create a table with counts
+pdata<-data.frame(table(d$What.is.your.preferred.gender.pronoun., d$Program))
+names(pdata)<-c("Gender","Program", "Count")
+
+#format the x tick labels so that they wrap to the next line and not bleed
+levels(pdata$Program)<-gsub(" ","\n", levels(pdata$Program))
+
+#ggplot code
+ggplot(data=pdata, aes(x=reorder(Program,-Count), y=Count, fill = Gender)) + 
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  ggtitle("Distribution of class by Gender and Program") +
+  xlab("Program") +
+  ylab("Number of students in Program")
